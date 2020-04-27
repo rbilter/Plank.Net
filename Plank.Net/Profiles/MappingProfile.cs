04dac90a7f53018a973307323ebce355599b69a9
Rich Bilter <rbilter@rxsense.com>
2020-04-27 11:04:13 -0400

    Removed remaining code around caching, wasn't well thought out or well implemented

--- Plank.Net/Profiles/MappingProfile.cs
+++ Plank.Net/Profiles/MappingProfile.cs
@@ -1,9 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.Practices.EnterpriseLibrary.Validation;
 using Plank.Net.Contracts;
-using Plank.Net.Data;
 using Plank.Net.Models;
-using System.Linq;
 using X.PagedList;
 
 namespace Plank.Net.Profiles
@@ -15,9 +13,6 @@ namespace Plank.Net.Profiles
         public MappingProfile()
         {
             CreateMap<ValidationResult, PlankValidationResult>();
-
-            CreateMap<IPagedList<TEntity>, PagedListCache>()
-                .ForMember(dest => dest.Ids, opt => opt.MapFrom(src => src.Select(i => i.Id)));
 
             CreateMap<IPagedList<TEntity>, PlankEnumerableResponse<TEntity>>()
                 .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src));
